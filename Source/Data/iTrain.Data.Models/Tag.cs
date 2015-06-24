1d556eb80c9a365619b29bc1e61f081fc257ce7f
manutdfen4e <mkirilova90@gmail.com>
2015-06-24 16:28:28 +0300

    Created interfaces for repositories(IRepository, IDeletableEntityRepository)
    + Test entity Tag

---
+++ Source/Data/iTrain.Data.Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿namespace iTrain.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using iTrain.Data.Common.Models;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Tag: AuditInfo, IDeletableEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        public System.DateTime? DeletedOn { get; set; }
+    }
+}
